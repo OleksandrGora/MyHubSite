c50eea8d7acf45bb3c9b6cf7cd298cb1e37d3aa7
OlexabdrGora <olexandr.gora@gmail.com>
2023-05-15 13:44:25 +0300

    upd pages links

--- app.py
+++ app.py
@@ -7,6 +7,19 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/page1.html")
+def page1():
+    return render_template("page1.html")
+
+@app.route("/page2.html")
+def page2():
+    return render_template("page2.html")
+
+@app.route("/page3.html")
+def page3():
+    return render_template("page3.html")
+
+
 @app.route("/styles.css")
 def style():
     return render_template("styles.css")
